e5c5ab32f8eb93edd357a1f2a4bf4abee41382a5
tegardm <rikutaru98@gmail.com>
2023-08-15 08:31:11 +0700

    add vercel json new

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { loadUsers, addUsers, hapusUser, findUser, cekDuplikat, groupedData, downloadFile, deleteAll } = require('./utils/methods')
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
